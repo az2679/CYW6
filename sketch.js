7c253f56960132a48c53b0a74cf9c340dd07d79a
Athena <az2679@nyu.edu>
2023-03-06 16:02:08 -0500

    cleaned refactored

--- sketch.js
+++ sketch.js
@@ -12,14 +12,12 @@ function draw() {
   
   cellSize = min(width / 10, height / 10);
   
-  //I think this is fine as is, wouldn't benefit much from refactoring
-  //rectangular grid in background, recode decode 1
+//rectangle background
   for (let x = cellSize -50; x < width; x += cellSize) {
     for (let y = cellSize -50; y < height; y += cellSize) {
     push()
     translate(x,y)
-    // rotate(0.5)
-      
+    // rotate(0.5) 
     let chance = random(1);
     if (chance < 0.5) {
       sign = 1;
@@ -27,8 +25,7 @@ function draw() {
       sign = -1;
     }
     let angle = random(0, -60 * sign);
-    rotate(angle);
-      
+    rotate(angle); 
     stroke(255)
     fill('rgba(210,209,211,0.41)')
     rect(0, 0, 20, 10)
@@ -36,86 +33,25 @@ function draw() {
     }
   } 
   
-  let grid3 = new squareGrid(350, 0, 75, width/15, 15, 'rgb(37,54,93)', 'rgb(190,72,92)')
-  grid3.show()
-  //   //vertical grid
-  // for(let x=350; x<width-150; x+=15){
-  //   for (let y=0; y<height; y+=15){
-  //   let startColor = color('rgb(37,54,93)')
-  //   let endColor = color('rgb(190,72,92)');
-  //   let grad = lerpColor(startColor, endColor, x/y*0.2)
-  //   fill(grad) 
-  //   strokeWeight(0.01)
-  //   stroke(255)
-  //   rect(x, y, 15)
-  //   strokeWeight(1)
-  //   }
-  // }
+  //back wall grid
+    let grid3 = new squareGrid(350, 0, 75, width/15, 15, 'rgb(37,54,93)', 'rgb(190,72,92)')
+    grid3.show()
 
-  diagonalSticks(width*0.05, height*0.5, 'rgb(196,32,60)', 'rgba(225,220,229,0.97)')
-  //  //pink rotated columns
-  // for(let x=width*0.05; x<width*1.4; x+=40){
-  //   for (let y=height*0.5; y<height*0.7; y+=60){
-  //   push()
-  //   translate(110, -200)
-  //   rotate(0.5)
-  //   let startColor = color('rgb(196,32,60)');
-  //   let endColor = color('rgba(225,220,229,0.97)');
-  //   let grad = lerpColor(startColor, endColor, x/y*0.5)
-  //   stroke('rgba(227,223,231,0.54)')
-  //   fill(grad)
-  //   rect(x, y, 30, 60)
-  //   rect(x, y-50, 30, 60)
-  //   rect(x, y+100, 30, 60)
-  //   pop()
-  //   }
-  // }
+  //pink rotated columns
+    diagonalSticks(width*0.05, height*0.5, 'rgb(196,32,60)', 'rgba(225,220,229,0.97)')
   
-  let grid1 = new horizontalGrid(100, 50, 80, 40, 'rgb(190,72,92)', 'rgb(37,54,93)')
-  grid1.show()
-  //   //base, large grid below
-  // for (let y=50; y<400; y+=40){
-  //   for(let x=100; x < 400; x+=40){
-  //   let startColor = color('rgb(190,72,92)')
-  //   let endColor = color('rgb(37,54,93)');
-  //   let grad = lerpColor(startColor, endColor, x/y*0.6)
-  //   fill(grad) 
-  //   stroke(0)
-  //   rect(x, y, 80,40)
-  //   }
-  // }
+  //base, large grid below
+    let grid1 = new horizontalGrid(100, 50, 80, 40, 'rgb(190,72,92)', 'rgb(37,54,93)')
+    grid1.show()
 
-  let grid2 = new verticalGrid(150, 100, 30, 60, 'rgb(36,62,122)', 150)
-  grid2.show()
-  //  //light blue grid on top
-  // for(let x=150; x<300; x+=30){
-  //   for (let y=100; y<250; y+=30){
-  //   let startColor = color('rgb(36,62,122)');
-  //   let endColor = color(150);
-  //   let grad = lerpColor(startColor, endColor, x/y*0.3)
-  //   fill(grad)
-  //   stroke(255)
-  //   rect(x, y, 30, 60)
-  //   }
-  // }
+  //light blue grid on top
+    let grid2 = new verticalGrid(150, 100, 30, 60, 'rgb(36,62,122)', 150)
+    grid2.show()
   
-  
-  let grid4 = new rotatedGrid(200, 75, 5, 3, 12, 'rgba(213,209,218,0.95)', 'rgba(213,209,218,0.95)')
-  grid4.show()
-  // //rotated rectangles, recode decode 1
-  //  for(let x=200; x<250; x+=12){
-  //   for (let y=75; y<110; y+=12){
-  //     push()
-  //     translate(x,y)
-  //     rotate(1)
-  //     fill('rgba(213,209,218,0.95)')
-  //     rect(0, 0, 10)
-  //     pop()
-  //   }
-  // }
+  //rotated rectangles
+    let grid4 = new rotatedGrid(200, 75, 5, 3, 12, 'rgba(213,209,218,0.95)', 'rgba(213,209,218,0.95)')
+    grid4.show()
 
-
-  //I thought about making a loop but I need to specify the colors. >> i could use method with gradSticks but I don't really need to repeat this part.
   //color palette
     let from = color('rgb(190,72,92)')
     let to = color('rgb(37,54,93)');
@@ -130,48 +66,12 @@ function draw() {
     fill(to);
     rect(80, height-70, 20, 60);
 
-  
-  solidSticks(width*0.35, height*0.55, 255)
-  //  //white columns
-  // for(let x=width*0.35; x<width*0.9; x+=60){
-  //   for (let y=height*0.55; y<height*0.75; y+=60){
-  //     fill(255)
-  //     rect(x, y+5, 30, 60)
-  //     }
-  //   }
-  
-  gradSticks(width*0.45, height*0.55, 'rgb(214,53,80)', 'rgb(3,29,88)')
-  // //for 3 gradient sticks
-  //   strokeWeight(1.5)
-  //   let stickStart = color('rgb(214,53,80)')
-  //   let stickEnd = color('rgb(3,29,88)');
-  //   let stickMiddle1 = lerpColor(stickStart, stickEnd, 0.25);
-  //   let stickMiddle2 = lerpColor(stickStart, stickEnd, 0.5);
-  //   let stickMiddle3 = lerpColor(stickStart, stickEnd, 0.75);
-  //   stroke('rgba(213,209,218,0.54)')
-  //
-  //   fill(stickMiddle2);
-  //   rect(width*0.45, (height*0.55)+0, 30, 60);
-  //   fill(stickMiddle3);
-  //   rect(width*0.45, (height*0.55)+60, 30, 60);
-  //   fill(stickEnd);
-  //   rect(width*0.45, (height*0.55)+120, 30, 60);
-  //
-  //   fill(stickMiddle1);
-  //   rect((width*0.45)+60, (height*0.55)+0, 30, 60);
-  //   fill(stickMiddle2);
-  //   rect((width*0.45)+60, (height*0.55)+60, 30, 60);
-  //   fill(stickMiddle3);
-  //   rect((width*0.45)+60, (height*0.55)+120, 30, 60);
-  //
-  //   fill(stickStart);
-  //   rect((width*0.45)+120, (height*0.55)+0, 30, 60);
-  //   fill(stickMiddle1);
-  //   rect((width*0.45)+120, (height*0.55)+60, 30, 60);
-  //   fill(stickMiddle2);
-  //   rect((width*0.45)+120, (height*0.55)+120, 30, 60);
-  // strokeWeight(1)
-  
+  //white columns
+    solidSticks(width*0.35, height*0.55, 255)
+
+  //3 gradient sticks
+    gradSticks(width*0.45, height*0.55, 'rgb(214,53,80)', 'rgb(3,29,88)')
+
 
   noLoop()
 }
